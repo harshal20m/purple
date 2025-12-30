64bbf654bc8c3c0bb98409d94f4f54b72df732b8
harshal mali <20harshalmali@gmail.com>
2025-12-30 15:49:22 +0530

    Update CORS origin to allow requests from the new frontend URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.set("trust proxy", 1);
 app.use(helmet()); // Set security headers
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5137",
     credentials: true,
   })
 );
